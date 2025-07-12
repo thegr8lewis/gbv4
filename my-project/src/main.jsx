3b2acc1c79534567de4ab7c423e1d29f42c66604
thegr8lewis <lewis.nyakaru@gmail.com>
2025-07-13 01:38:56 +0300

    3t24t

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,29 +1,29 @@
 
 
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App/>
-//     </BrowserRouter>
-//   </StrictMode>
-// )
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'  // Changed from BrowserRouter to HashRouter
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>,
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </StrictMode>
 )
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { HashRouter } from 'react-router-dom'  // Changed from BrowserRouter to HashRouter
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   </StrictMode>,
+// )
